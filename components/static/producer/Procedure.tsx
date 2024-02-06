713b3c585eb82ee58b06d89205ff42317e99bf74
github-usman <usmantoyou@gmail.com>
2024-02-06 10:42:12 +0530

    producer arrow desgin fix

--- components/static/producer/Procedure.tsx
+++ components/static/producer/Procedure.tsx
@@ -20,35 +20,35 @@ export const Procedure = () => {
       <p className='main-para mb-0'>Following is the list of all the vital documents required for the Registration of Producer Company in India:</p>
       <div className="main flex justify-content-between align-center m-auto mobile-flex-column">
         <div className='flex-5'>
-          <button onClick={() => handleActive('1')} className={`btn flex ${active === '1' ? 'active-btn' : 'none-btn'}`}>
-            <div>
-              <Image src={one} alt="one" loading="lazy" width={30} height={30} /> &nbsp; Step 1: Obtain DSC
+          <button onClick={() => handleActive('1')} className={`btn flex ${active === '1' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2' >
+              <Image src={one} alt=" Select the Company name" loading="lazy" width={30} height={30} /><div className='pe-4'> Step 1: Obtain DSC</div>
+              <Image src={arrow} alt="pu step 1" loading="lazy" width={30} height={30} className="position-absolute end-0" />
             </div>
-            <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} />
           </button>
-          <button onClick={() => handleActive('2')} className={`btn flex ${active === '2' ? 'active-btn' : 'none-btn'}`}>
-            <div>
-              <Image src={two} alt="two" loading="lazy" width={30} height={30} /> &nbsp; Step 2: Choose a Unique Company Name
+          <button onClick={() => handleActive('2')} className={`btn flex ${active === '2' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2' >
+              <Image src={two} alt=" Select the Company name" loading="lazy" width={30} height={30} /><div className='pe-4'> Step 2: Choose a Unique Company Name</div>
+              <Image src={arrow} alt="pu step 1" loading="lazy" width={30} height={30} className="position-absolute end-0" />
             </div>
-            <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} />
           </button>
-          <button onClick={() => handleActive('3')} className={`btn flex ${active === '3' ? 'active-btn' : 'none-btn'}`}>
-            <div>
-              <Image src={three} alt="three" loading="lazy" width={30} height={30} /> &nbsp; Step 3: Prepare all Requisite Documents
+          <button onClick={() => handleActive('3')} className={`btn flex ${active === '3' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2' >
+              <Image src={three} alt=" Select the Company name" loading="lazy" width={30} height={30} /><div className='pe-4'> Step 3: Prepare all Requisite Documents</div>
+              <Image src={arrow} alt="pu step 1" loading="lazy" width={30} height={30} className="position-absolute end-0" />
             </div>
-            <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} />
           </button>
-          <button onClick={() => handleActive('4')} className={`btn flex ${active === '4' ? 'active-btn' : 'none-btn'}`}>
-            <div>
-              <Image src={four} alt="four" loading="lazy" width={30} height={30} /> &nbsp; Step 4: File for Incorporation
+          <button onClick={() => handleActive('4')} className={`btn flex ${active === '4' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2' >
+              <Image src={four} alt=" Select the Company name" loading="lazy" width={30} height={30} /><div className='pe-4'> Step 4: File for Incorporation</div>
+              <Image src={arrow} alt="pu step 1" loading="lazy" width={30} height={30} className="position-absolute end-0" />
             </div>
-            <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} />
           </button>
-          <button onClick={() => handleActive('5')} className={`btn flex ${active === '5' ? 'active-btn' : 'none-btn'}`}>
-            <div>
-              <Image src={five} alt="five" loading="lazy" width={30} height={30} /> &nbsp; Step 5: Follow up and Approval
+          <button onClick={() => handleActive('5')} className={`btn flex ${active === '5' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2' >
+              <Image src={five} alt=" Select the Company name" loading="lazy" width={30} height={30} /><div className='pe-4'> Step 5: Follow up and Approval</div>
+              <Image src={arrow} alt="pu step 1" loading="lazy" width={30} height={30} className="position-absolute end-0" />
             </div>
-            <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} />
           </button>
         </div>
         <div className='flex-5 text-justify fs-20'>
