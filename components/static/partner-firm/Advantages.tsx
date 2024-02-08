8cf2095b4141a1e67aedec4e2a2974588955212a
github-usman <usmantoyou@gmail.com>
2024-02-08 09:52:58 +0530

    register-firm:update the design section arrow button heading,mobile veiw

--- components/static/partner-firm/Advantages.tsx
+++ components/static/partner-firm/Advantages.tsx
@@ -48,25 +48,25 @@ export const Advantages = () => {
         </div>
         <div className='flex-5 text-justify fs-20 p-color-black'>
           <div className={`${active === '1' ? 'active' : 'none'}`}>
-            <h3>Easy to Incorporate: </h3>
+            <h3>1: Easy to Incorporate </h3>
             <p>
             In comparison to other types of business organisations, forming a partnership firm is simple. By preparing the partnership deed and entering into the partnership agreement, the partnership firm can be formed. Other than the partnership agreement, no other documents are necessary. It is not even required to be registered with the Registrar of Firms. A partnership firm can be created and registered at a later date because registration is optional.
             </p>
           </div>
           <div className={`${active === '2' ? 'active' : 'none'}`}>
-            <h3>Less Compliance:</h3>
+            <h3>2: Less Compliance</h3>
             <p>
              In comparison to a corporation or an LLP, a partnership firm is subject to far fewer regulations. The partners do not require a Digital Signature Certificate (DSC) or a Director Identification Number (DIN), which are required for LLP company directors or designated partners. Any changes to the business can be readily implemented by the partners. Their operations are subject to legal constraints. It is less expensive to establish than a corporation or limited liability partnership. The dissolution of a partnership firm is simple and requires few legal requirements.
             </p>
           </div> 
           <div className={`${active === '3' ? 'active' : 'none'}`}>
-            <h3>Quick Decision: </h3>
+            <h3>3: Quick Decision</h3>
             <p>
              Because there is no distinction between ownership and management in a partnership firm, decision-making is swift. All choices are made collaboratively by the partners and can be applied instantly. The partners have broad powers and actions that they can carry out on behalf of the company. They can even conduct transactions on behalf of the partnership firm without the agreement of the other partners.
             </p>
           </div>
           <div className={`${active === '4' ? 'active' : 'none'}`}>
-            <h3>Sharing of Profits and Losses:</h3>
+            <h3>4: Sharing of Profits and Losses</h3>
             <p>
             The partners split the firm’s profits and losses evenly. They can even choose their own profit and loss ratio in the partnership firm. They feel a sense of ownership and accountability because the firm’s profitability and turnover are based on their efforts. Any loss incurred by the firm will be shared equally or in accordance with the partnership deed ratio, alleviating the weight of loss on one individual or partner. They are jointly and severally accountable for the firm’s operations.
             </p>
