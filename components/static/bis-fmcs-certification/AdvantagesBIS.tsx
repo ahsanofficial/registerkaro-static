1187949627962f6bb6ab687c5593be94e27a11ec
github-usman <usmantoyou@gmail.com>
2024-03-08 14:01:30 +0530

    bis-fmcs-certification-dummy : design updated

--- components/static/bis-fmcs-certification/AdvantagesBIS.tsx
+++ components/static/bis-fmcs-certification/AdvantagesBIS.tsx
@@ -1,104 +1,71 @@
-
-import Image from 'next/image'
-import React, { useState } from 'react'
-import one from '../../../assets/images/pvt-reg/HowToRegister/one.svg'
-import two from '../../../assets/images/pvt-reg/HowToRegister/two.svg'
-import three from '../../../assets/images/pvt-reg/HowToRegister/three.svg'
-import four from '../../../assets/images/pvt-reg/HowToRegister/four.svg'
-import five from '../../../assets/images/pvt-reg/HowToRegister/five.svg'
-import six from '../../../assets/images/pvt-reg/HowToRegister/six.svg'
-
-
-import arrow from '../../../assets/images/pvt-reg/HowToRegister/arrow.svg'
+import Image from "next/image";
+import bft from "../../../assets/images/pvt-reg/benefit.svg";
 
 export const AdvantagesBIS = () => {
-  const [active, setActive] = useState('1')
-
-  const handleActive = (id: string) => {
-    setActive(id)
-  }
-
   return (
-    <div className='DocumentButtonWise w-80 m-auto' id='AdvantagesBIS'>
-        <div className='flex justify-content-center'>
-        <p className='main-para mb-2'>It is mandatory for Foreign Manufacturers to nominate an AIR or Authorized Indian Representative in India to obtain a License. The Authorized Indian Representative serves as the local representative responsible for managing and submitting the Certification application, as well as addressing any inquiries. Before nominating the AIR, the following points must be adhered to:</p>
-      </div>
-      <div className="main flex justify-content-between align-center m-auto mobile-flex-column-reverse">
-     
-        <div className='flex-5 text-justify fs-20 p-color-black'>
-          <div className={`${active === '1' ? 'active' : 'none'}`}>
-            <h3>1: Indian Residency Requirement:</h3>
-            <p className='mb-0'>
-             The nominated Authorized Indian Representative must be an Indian resident.
-            </p>
-          </div>
-          <div className={`${active === '2' ? 'active' : 'none'}`}>
-            <h3>2: Zero Conflict of Interest:</h3>
-            <p className='mb-0'>The Authorized Indian Representative should have no conflicts of interest related to the testing of a product sample in third-party labs.
-            </p>
-          </div>
-          <div className={`${active === '3' ? 'active' : 'none'}`}>
-            <h3>3: Inclusion in License Documents:</h3>
-            <p className='mb-0'>The name of the Authorized Indian Representative must be included in the License documents.
-            </p>
-          </div>
-          <div className={`${active === '4' ? 'active' : 'none'}`}>
-            <h3>4: Minimum Qualification Criteria:</h3>
-            <p className='mb-0'>The minimum qualification for an Authorized Indian Representative is a bachelor’s degree, along with knowledge of the provisions of the BIS Act, 2016, Rules, and Regulations.
-            </p>
-          </div>
-          <div className={`${active === '5' ? 'active' : 'none'}`}>
-            <h3>5: Consent and Responsibility:</h3>
-            <p className='mb-0'>The Authorized Indian Representative must provide explicit consent and acknowledge responsibility for compliance with the provisions of the BIS Act, Terms and Conditions, and Rules & Regulations specified in the BIS License.
-            </p>
-          </div>
-          <div className={`${active === '6' ? 'active' : 'none'}`}>
-            <h3>6: Exclusive Representation:</h3>
-            <p className='mb-0'>An Authorized Indian Representative can represent only one Foreign Manufacturer at a given time and is not allowed to represent other foreign manufacturers concurrently.
-            </p>
-          </div>
-          
+    <div id="AdvantagesBIS">
+      <p className="main-para mb-5">
+        It is mandatory for Foreign Manufacturers to nominate an AIR or
+        Authorized Indian Representative in India to obtain a License. The
+        Authorized Indian Representative serves as the local representative
+        responsible for managing and submitting the Certification application,
+        as well as addressing any inquiries. Before nominating the AIR, the
+        following points must be adhered to:
+      </p>
+      <div className="benefits flex w-80 m-auto mobile-flex-column-reverse">
+       
+        <div className="flex-7">
+          <p>
+            <strong>Indian Residency Requirement:</strong>
+            The nominated Authorized Indian Representative must be an Indian
+            resident.
+          </p>
+
+          <p>
+            <strong>Zero Conflict of Interest:</strong>
+            The Authorized Indian Representative should have no conflicts of
+            interest related to the testing of a product sample in third-party
+            labs.
+          </p>
+
+          <p>
+            <strong>Inclusion in License Documents:</strong>
+            The name of the Authorized Indian Representative must be included in
+            the License documents.
+          </p>
+
+          <p>
+            <strong>Minimum Qualification Criteria:</strong>
+            The minimum qualification for an Authorized Indian Representative is
+            a bachelor’s degree, along with knowledge of the provisions of the
+            BIS Act, 2016, Rules, and Regulations.
+          </p>
+
+          <p>
+            <strong>Consent and Responsibility:</strong>
+            The Authorized Indian Representative must provide explicit consent
+            and acknowledge responsibility for compliance with the provisions of
+            the BIS Act, Terms and Conditions, and Rules & Regulations specified
+            in the BIS License.
+          </p>
+
+          <p>
+            <strong>Exclusive Representation:</strong>
+            An Authorized Indian Representative can represent only one Foreign
+            Manufacturer at a given time and is not allowed to represent other
+            foreign manufacturers concurrently.
+          </p>
         </div>
-        <div className='flex-5'>
-          <button onClick={() => handleActive('1')} className={`btn flex ${active === '1' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={one} alt="three" loading="lazy" width={30} height={30}  /><div className='ms-1 pe-4'>Indian Residency Requirement:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
-          <button onClick={() => handleActive('2')} className={`btn flex ${active === '2' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={two} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Zero Conflict of Interest:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
-          <button onClick={() => handleActive('3')} className={`btn flex ${active === '3' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={three} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Inclusion in License Documents:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
-          <button onClick={() => handleActive('4')} className={`btn flex ${active === '4' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={four} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Minimum Qualification Criteria:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
-          <button onClick={() => handleActive('5')} className={`btn flex ${active === '5' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={five} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Consent and Responsibility:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
-          <button onClick={() => handleActive('6')} className={`btn flex ${active === '6' ? 'active-btn' : 'none-btn'} position-relative`}>
-            <div className='d-flex justify-content-between align-items-center gap-2'>
-              <Image src={six} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Exclusive Representation:</div> 
-              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
-            </div>
-          </button>
+        <div className="flex-4 mt-5">
+          <Image
+            alt="The benefits of private limited company in india and these are Separate Legal Entity, Ease in Raising Funds, Limited Liability, Easy Transferable, Limited Liability and ESOP"
+            className="benefits-img w-100"
+            height={550}
+            loading="lazy"
+            src={bft}
+          />
         </div>
       </div>
     </div>
-  )
-  
-}
+  );
+};
