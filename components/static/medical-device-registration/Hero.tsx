028105ade8c33d55473a995c2c8ba38aee6e6667
github-usman <usmantoyou@gmail.com>
2024-03-19 15:52:41 +0530

    medical-device : package inclusion update

--- components/static/medical-device-registration/Hero.tsx
+++ components/static/medical-device-registration/Hero.tsx
@@ -7,12 +7,10 @@ import { GoogleDiv } from '../GoogleDiv';
 
 let hero = {
     "pack_includes": [
-        "2 DIN and DSC for two Directors",
-        "Drafting of MoA & AoA",
-        "Registration fees and stamp duty",
-        "Company Incorporation Certificate",
-        "Company PAN and TAN",
-        "ZohoBooks subscription"
+        "Consultancy with Expert",
+        "Documents preparation",
+        "Follow up with Department",
+        "Registration Certificate",
     ]
 }
 
