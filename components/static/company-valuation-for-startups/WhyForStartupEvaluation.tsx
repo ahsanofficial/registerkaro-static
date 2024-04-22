c53cc897e30ead72dc7fbcee3b1d68ce4b9e113a
Aman Gupta <khwaabknight@gmail.com>
2024-04-22 19:01:42 +0530

    company-valuation-for-startups-dummy : unordered list to ordered list

--- components/static/company-valuation-for-startups/WhyForStartupEvaluation.tsx
+++ components/static/company-valuation-for-startups/WhyForStartupEvaluation.tsx
@@ -10,13 +10,13 @@ export const WhyForStartupEvaluation = () => {
           <Image
             alt="Factors Affecting Company Valuation for Startups in India"
             className="WhyForStartupEvaluation-img w-100"
-            height={480}
+            height={600}
             loading="lazy"
             src={bft}
           />
         </div>
         <div className="flex-7">
-          <ul>
+          <ol>
             <li>A Startup is a new business that is potentially fast expanding & tries to fill a void in the Indian Market by producing & facilitating a new and original product, process or service, but is currently facing challenges.</li>
             <li>RegisterKaro is a Company that also provide services for Company Valuation for Startups. We can help with everything from providing advice in the initial phase to ensuring that correct and right results are produced & precise financial analysis is made.</li>
             <li>At RegisterKaro, we specialize in precise startup valuation services tailored for the Indian market. Our seasoned team of valuation experts ensures accurate assessments across diverse industries, focusing particularly on tech startups and early-stage ventures.</li>
@@ -24,7 +24,7 @@ export const WhyForStartupEvaluation = () => {
             <li>Trusted by clients nationwide, we deliver top-notch startup valuation services in major Indian cities. Our startup valuation calculators streamline the assessment process, providing efficient and accurate results for thorough financial analysis.</li>
             <li>At RegisterKaro, we take pride in delivering the highest level of accuracy in startup valuation, supporting the growth of innovative ventures in India. By availing our services, entrepreneurs can sidestep the pitfalls of inaccurate valuation methods, ensuring a smooth business journey without penalties or complications.</li>
             <li>Choose RegisterKaro for prompt and effective services, enabling you to manage your business seamlessly and avoid the negative outcomes associated with faulty valuation methods.</li>
-          </ul>
+          </ol>
         </div>
         
       </div>
