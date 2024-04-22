c0acb5bae50777f0e7fb61d3ca237ce2b1369a10
Aman Gupta <khwaabknight@gmail.com>
2024-04-22 18:21:03 +0530

    international-trademark-registration : static revieq follow-up

--- components/static/international-trademark-registration/CheckIfCanApply.tsx
+++ components/static/international-trademark-registration/CheckIfCanApply.tsx
@@ -10,18 +10,18 @@ export const CheckIfCanApply = () => {
           <Image
             alt="Check if you can apply for the International Trademark Registration"
             className="CheckIfCanApply-img w-100"
-            height={200}
+            height={250}
             loading="lazy"
             src={bft}
           />
         </div>
         <div className="flex-7">
           <p>If you fall to be one among the following, then you are eligible for the International Trademark Registration:</p>
-          <ul>
+          <ol>
             <li>Any person or national of a nation that is a party to an agreement or protocol may apply for international registration.</li>
             <li>Any natural person or legal entity that resides in a nation covered by one of the treaties or has a legitimate and operational industrial or commercial presence there may apply.</li>
             <li>According to the Agreement, a registration must serve as the foundation for an application for worldwide registration.</li>
-          </ul>
+          </ol>
         </div>        
       </div>      
     </div>
