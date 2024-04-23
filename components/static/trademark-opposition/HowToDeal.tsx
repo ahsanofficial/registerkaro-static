71baa946ca800a1ccb3127139334a07a3b002aa1
Aman Gupta <khwaabknight@gmail.com>
2024-04-23 15:47:47 +0530

    trademark-opposition-dummy : static page added

---
+++ components/static/trademark-opposition/HowToDeal.tsx
@@ -0,0 +1,77 @@
+
+import Image from 'next/image'
+import { useState } from 'react'
+import four from '../../../assets/images/pvt-reg/HowToRegister/four.svg'
+import one from '../../../assets/images/pvt-reg/HowToRegister/one.svg'
+import three from '../../../assets/images/pvt-reg/HowToRegister/three.svg'
+import two from '../../../assets/images/pvt-reg/HowToRegister/two.svg'
+
+
+import arrow from '../../../assets/images/pvt-reg/HowToRegister/arrow.svg'
+
+export const HowToDeal = () => {
+  const [active, setActive] = useState('1')
+
+  const handleActive = (id: string) => {
+    setActive(id)
+  }
+
+  return (
+    <div className='DocumentButtonWise w-80 m-auto' id='HowToDeal'>
+      <div className="main flex justify-content-between align-center m-auto mobile-flex-column">
+        <div className='flex-5'>
+          <button onClick={() => handleActive('1')} className={`btn flex ${active === '1' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2'>
+              <Image src={one} alt="three" loading="lazy" width={30} height={30}  /><div className='ms-1 pe-4'>Step 1. Opposition Notice and counter statement:</div> 
+              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
+            </div>
+          </button>
+          <button onClick={() => handleActive('2')} className={`btn flex ${active === '2' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2'>
+              <Image src={two} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Step 2. Response to counter-statement with evidence:</div> 
+              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
+            </div>
+          </button>
+          <button onClick={() => handleActive('3')} className={`btn flex ${active === '3' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2'>
+              <Image src={three} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Step 3: Evidence in response: </div> 
+              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
+            </div>
+          </button>
+          <button onClick={() => handleActive('4')} className={`btn flex ${active === '4' ? 'active-btn' : 'none-btn'} position-relative`}>
+            <div className='d-flex justify-content-between align-items-center gap-2'>
+              <Image src={four} alt="three" loading="lazy" width={30} height={30} /><div className='ms-1 pe-4'>Step 4: Hearing: </div> 
+              <Image src={arrow} alt="arrow" loading="lazy" width={30} height={30} className='position-absolute end-0'/>
+            </div>
+          </button>
+        </div>
+        <div className='flex-5 text-justify fs-20 p-color-black'>
+          <div className={`${active === '1' ? 'active' : 'none'}`}>
+            <h3>Step 1. Opposition Notice and counter statement:</h3>
+            <p className='mb-0'> Firstly, you have to file a counter statement against the notice you received. Generally, you receive the notice within four months of trademark advertisement or re-advertisement in the Indian Trade Marks Journal. The notice is filed using form TM-O either online or offline by paying the prescribed fee along with the ground of opposition. You now have to file a counter statement defending the ground used, along with evidence, within two months from the date of such receipt. A counter statement filed later than two months will lead to abandonment of the application as there is no extension available to file the Counter Statement.
+            </p>
+          </div>
+          <div className={`${active === '2' ? 'active' : 'none'}`}>
+            <h3>Step 2. Response to counter-statement with evidence:</h3>
+            <p className='mb-0'> Upon the receipt of the counter statement, the opposing party is required to respond within two months, along with evidence to support their claims made in the Notice of Opposition, which can also include an affidavit and other evidence. It is suggested to reply to the counter statement; however, the response is not mandatory and can be waived off by the opposing party by informing the Trade Marks Registry in India within the prescribed time, or else, the proceedings are tagged as abandoned.
+            </p>
+          </div>
+          <div className={`${active === '3' ? 'active' : 'none'}`}>
+            <h3>Step 3: Evidence in response: </h3>
+            <p className='mb-0'>If chosen to respond with the supportive evidence by the opposing party, the applicant can also file the evidence in support of his counter statement, or else can choose to waive off the opportunity by filing a letter to the Trademark Registry of India. Otherwise, one should file the evidence within one month.
+            </p>
+          </div>
+          <div className={`${active === '4' ? 'active' : 'none'}`}>
+            <h3>Step 4: Hearing: </h3>
+            <p className='mb-0'>Upon the successful documentation process, the Registrar shall provide the parties the opportunity to be heard and present their case before the Registrar as per Section 21(5) of the Act. The registrar notifies the parties regarding the hearing and the date for the same. Each party presents its case with evidence to support it before the registrar. On evaluation, the registrar renders a judgement in support of one party, stating whether the trademark shall be registered or denied.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+  
+}
+
+
---
+++ components/static/trademark-opposition/Process.tsx
@@ -0,0 +1,47 @@
+import certificate from '../../../assets/images/static/qatar-company-registration/certificate.svg';
+import Image from 'next/image';
+
+export const Process = () => {
+    return (
+        <div className='process flex w-80 m-auto mobile-flex-column-reverse'>
+            <div className='flex-5'>
+                <p className='text-justify'>A legal challenge made against a trademark’s registration by a third party is known as trademark opposition. Through this procedure, third parties can object to the registration of a new trademark application that they feel may violate their rights to an existing brand or confuse customers by being confusingly similar to an already-registered trademark. The Trademark opposition is filed within four months from the date of advertisement of the re-advertisement of the trademark in the Indian Trademark Journal by filing a form TM-O along with the prescribed fee.</p>
+                
+                <div className='page-point'>
+                    <ul>
+                        <ul>
+                            <a href="#WhoCanOppose" className="scrollTo">
+                                <li>Who can oppose the trademark?</li>
+                            </a>                            
+                            <a href="#WhenRecieveOpposition" className="scrollTo">
+                                <li>When do you receive the Trademark Opposition?</li>
+                            </a>
+                            <a href="#HowToDeal" className="scrollTo">
+                             <li>How to deal with Trademark Opposition?</li>
+                            </a>
+                            <a href="#DifferenceOppositionObjection" className="scrollTo">
+                             <li>How is trademark opposition different from Trademark Objection?</li>
+                            </a>
+                            <a href="#WhyChoose" className="scrollTo">
+                             <li>How does RegisterKaro help with the Trademark Opposition?</li>
+                            </a>
+                            <a href="#Why" className="scrollTo">
+                                <li>Why RegisterKaro?</li>
+                            </a>
+                            <a href="#Faq" className="scrollTo">
+                                <li>Frequently Asked Questions</li>
+                            </a> 
+
+                        </ul>
+                    </ul>
+                </div>
+            </div>
+            <div className='flex-5'>
+                <p className="small-heading text-center">Company Registration in Qatar</p>
+                <div>
+                    <Image alt="What is Trademark Opposition?" className="certificate h-100 w-100" loading="lazy" src={certificate} />
+                </div>
+            </div>
+        </div>
+    )
+}
