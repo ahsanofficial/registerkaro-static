97fa2cc23dd0c2465b7498dfbcc0c16d4224c5ef
Aman Gupta <khwaabknight@gmail.com>
2024-04-29 13:14:59 +0530

    spice-board-registration-dummy : static page added

---
+++ components/static/spice-board-registration/Process.tsx
@@ -0,0 +1,47 @@
+import dft from "../../../assets/images/pvt-reg/benefit.svg";
+import Image from 'next/image';
+
+export const Process = () => {
+    return (
+        <div className='process flex w-80 m-auto mobile-flex-column-reverse'>
+            <div className='flex-5'>
+                <p className='text-justify'>Certification and supervision of quality. Exporter registration. Gathering and recording trade data. Offering advice to the Central Government regarding policies concerning the import and export of spices.</p>
+                <p className='text-justify'>Encouraging the exporting of spices and spice products. Quality control and supervision of exported goods. Improving production methods by conducting research in scientific, technological, and economic fields. Advice for farmers on achieving increased and improved crop yields through the implementation of scientific farming techniques. Supplying growers with financial and material assistance.</p>
+                
+                <div className='page-point'>
+                    <ul>
+                        <ul>
+                            <a href="#WhyBeneficial" className="scrollTo">
+                             <li>Why it’s Beneficial?</li>
+                            </a>
+                            <a href="#DocumentsRequired" className="scrollTo">
+                             <li>Documents Required</li>
+                            </a>
+                            <a href="#HowRegister" className="scrollTo">
+                             <li>How To Register?</li>
+                            </a>
+                            <a href="#ListOfSpices" className="scrollTo">
+                             <li>List Of Spices for Registration</li>
+                            </a>
+                            <a href="#WhyChoose" className="scrollTo">
+                             <li>How RegisterKaro helps you in the registration process?</li>
+                            </a>
+                            <a href="#Why" className="scrollTo">
+                                <li>Why RegisterKaro?</li>
+                            </a>
+                            <a href="#Faq" className="scrollTo">
+                                <li>Frequently Asked Questions</li>
+                            </a>
+                        </ul>
+                    </ul>
+                </div>
+            </div>
+            <div className='flex-5'>
+                <p className="small-heading text-center">Overview on BEE Registration in India</p>
+                <div>
+                    <Image alt="Overview on BEE Registration in India" className="certificate h-100 w-100" loading="lazy" src={dft} />
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ components/static/spice-board-registration/WhyBeneficial.tsx
@@ -0,0 +1,28 @@
+import dft from "../../../assets/images/pvt-reg/benefit.svg";
+import Image from 'next/image';
+
+export const WhyBeneficial = () => {
+    return (
+        <div id="WhyBeneficial">
+            
+            <div className='whyBeneficial flex w-80 m-auto mobile-flex-column'>
+                <div className='flex-5'>
+                    <div>
+                        <Image
+                            alt="Why it’s Beneficial?"
+                            className="WhyBeneficial-img w-100 Importance-img-h-100"
+                            height={250}
+                            loading="lazy"
+                            src={dft}
+                        />
+                    </div>
+                </div>
+                <div className="flex-7">
+                    <p>Promoting the cultivation and overseas sale of organic spices. Enabling infrastructure for processing and enhancing value. Registration and licensing required for all individuals exporting spices. Support for studying and researching ways to enhance processing practices, quality management systems, grading methods, and packaging techniques. Creation of advertising and informative content using different mediums to assist exporters and importers.</p>
+                    <p>Assists exporters and importers in making connections with each other. Finds reliable suppliers to meet the specific needs of importers. Sends trade inquiries from foreign countries to trusted exporters. Facilitates networking between Indian exporters and global buyers by coordinating their presence at key international trade events and gatherings. Review complaints from importers to implement corrective and preventive measures.</p>
+                    <p>Leads the program aimed at enhancing the quality of Indian spices. Oversees a thorough and current database for companies involved in exporting and importing. Facilitates communication among global organizations, exporters, and government officials via contact group initiatives. Showcases India at prominent global food events; hosts food expos and cooking workshops to create awareness.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
