4f384682f524be75c94acb74f60b81d9197035e5
Aman Gupta <khwaabknight@gmail.com>
2024-05-17 14:44:27 +0530

    added trademark-search dynamic

--- components/static/trademark-search/ApplicableRules.tsx
+++ components/static/trademark-search/ApplicableRules.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import bft from "../../../assets/images/static/trademark-search/Applicable_Rules.svg";
-
-const ApplicableRules = () => {
+interface HeroProps {
+    cityName: string;
+}
+const ApplicableRules = ({ cityName }: HeroProps) => {
   return (
     <div id="ApplicableRules">
        <Image
@@ -15,7 +17,7 @@ const ApplicableRules = () => {
       <div className="benefits flex w-80 m-auto ">
 
         <div className="flex-7">
-          <p><strong>The Trademarks Act, 1999:</strong> This is the primary legislation governing trademarks in India. It outlines the provisions related to the registration, protection, and enforcement of trademarks in the country.</p>  
+          <p><strong>The Trademarks Act, 1999:</strong> This is the primary legislation governing trademarks in {cityName}. It outlines the provisions related to the registration, protection, and enforcement of trademarks in the country.</p>  
           <p><strong>The Trademarks Rules, 2017:</strong> These rules provide detailed procedures and guidelines for various aspects of trademark registration and related processes, including conducting searches.</p>  
         </div>
       
--- components/static/trademark-search/WhatProcess.tsx
+++ components/static/trademark-search/WhatProcess.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import bft from "../../../assets/images/static/trademark-search/What_is_the_process_to_conduct_a_Trademark_IP_India_Public_Search.svg";
-
-const WhatProcess = () => {
+interface HeroProps {
+    cityName: string;
+}
+const WhatProcess = ({ cityName }: HeroProps) => {
   return (
     <div id="WhatProcess">
         <p className="main-para">There are many ways in which you can conduct a TM Search Online, but we are discussing the most common ways:</p>
@@ -12,7 +14,7 @@ const WhatProcess = () => {
             <p><strong>1. </strong>Searching for a Trademark Online if you are looking for details for an existing Trademark, conducting an online search is by far your best option;</p>
             <p><strong>2. </strong>The initial step in conducting an online search is to conduct a keyword search on Trademarks/brands that you think might be identical to yours;</p>
             <p><strong>3. </strong>If the name entered doesn’t match anything within your given search parameters, move on to;</p>
-            <p><strong>4. </strong>To determine if the name is on file with the Trademark Registry, check the Trademark Registry or conduct an IP India Public Search;</p>
+            <p><strong>4. </strong>To determine if the name is on file with the Trademark Registry, check the Trademark Registry or conduct an IP {cityName} Public Search;</p>
             <p><strong>5. </strong>Sometimes these databases will provide some insight into whether or not another company has filed for safeguarding their brand name against your use as well;</p>
             <p><strong>6. </strong>If someone else has filed for protection of their brand name against the use of yours, this will be noted in each database record & generally includes links to their respective application.</p>
         </div>
