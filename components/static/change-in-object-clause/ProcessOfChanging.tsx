450ab7edfbb7bedf28562d5d2cb6c7bbf58a3678
Aman Gupta <khwaabknight@gmail.com>
2024-05-21 13:24:40 +0530

    added images for change-in-object-clause-dummy

--- components/static/change-in-object-clause/ProcessOfChanging.tsx
+++ components/static/change-in-object-clause/ProcessOfChanging.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import dft from "../../../assets/images/pvt-reg/benefit.svg";
+import dft from "../../../assets/images/static/change-in-object-clause/two.svg";
 
 export const ProcessOfChanging = () => {
   return (
@@ -11,7 +11,7 @@ export const ProcessOfChanging = () => {
           <Image
             alt="Process of changing an object clause:"
             className="ProcessOfChanging-img w-100 Importance-img-h-100"
-            height={600}
+            height={650}
             loading="lazy"
             src={dft}
           />
