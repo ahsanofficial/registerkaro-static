0975498dfbe44f2fc26bc1527445800e85a6c5f7
Aman Gupta <khwaabknight@gmail.com>
2024-05-22 16:57:10 +0530

    page 142 dynamic

--- components/static/transfer-pricing-agreement/Why.tsx
+++ components/static/transfer-pricing-agreement/Why.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import dft from "../../../assets/images/static/transfer-pricing-agreement//three.svg";
-
-export const Why = () => {
+interface HeroProps {
+    cityName: string;
+}
+export const Why = ({ cityName }: HeroProps) => {
   return (
     <div id="Why">
       <div className="benefits flex w-80 m-auto mobile-flex-column">
