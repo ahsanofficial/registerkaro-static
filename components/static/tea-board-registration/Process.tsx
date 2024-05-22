7e0de207d7708a14b520dad84aaa0c88f2d20b8a
Aman Gupta <khwaabknight@gmail.com>
2024-05-22 19:53:50 +0530

    [Dynamic] page 150 tea-board-registration

--- components/static/tea-board-registration/Process.tsx
+++ components/static/tea-board-registration/Process.tsx
@@ -1,17 +1,19 @@
 import certificate from '../../../assets/images/static/tea-board-registration/What is Tea Board of India_.svg';
 import Image from 'next/image';
-
-export const Process = () => {
+interface HeroProps {
+    cityName: string;
+}
+export const Process = ({ cityName }: HeroProps) => {
     return (
         <div className='process flex w-80 m-auto mobile-flex-column-reverse'>
             <div className='flex-5'>
-                <p className='text-justify'>India is the second largest producer of tea globally. It produces a wide range of tea such as Black, Herbal, Green, Nilgiri, Assam, etc. In terms of value, India’s total tea exports stood at a whopping USD 687.9 million, for the year 2021 and in terms of volume, it stood at 201 million kg. Therefore considering the ever-increasing global demand, exporting is an excellent opportunity for exporters as well as tea procedures of India and for this exact purpose, the Tea Board of India was set up in 1954, through the Tea Act of 1953.</p>
+                <p className='text-justify'>{cityName} is the second largest producer of tea globally. It produces a wide range of tea such as Black, Herbal, Green, Nilgiri, Assam, etc. In terms of value, {cityName}'s' total tea exports stood at a whopping USD 687.9 million, for the year 2021 and in terms of volume, it stood at 201 million kg. Therefore considering the ever-increasing global demand, exporting is an excellent opportunity for exporters as well as tea procedures of {cityName} and for this exact purpose, the Tea Board of {cityName} was set up in 1954, through the Tea Act of 1953.</p>
                 
                 <div className='page-point'>
                     <ul>
                         <ul>
                             <a href="#WhatIsTeaBoard" className="scrollTo">
-                             <li>What is Tea Board of India?</li>
+                             <li>What is Tea Board of {cityName}?</li>
                             </a>
                             <a href="#TypesOfLicenses" className="scrollTo">
                              <li>Types of Licenses granted by the Tea Board</li>
@@ -36,7 +38,7 @@ export const Process = () => {
                 </div>
             </div>
             <div className='flex-5'>
-                <p className="small-heading text-center">What is Tea Board of India?</p>
+                <p className="small-heading text-center">What is Tea Board of {cityName}?</p>
                 <div>
                     <Image alt="What is Tea Board of India?" className="certificate h-100 w-100" loading="lazy" src={certificate} />
                 </div>
